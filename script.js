c22e60056549955d4721f5af26d3197635f8350f
Sandeep_kumar136 <Ksundip750#gmail.com>
2023-12-15 21:54:18 +0530

    manipulating blog base page

--- script.js
+++ script.js
@@ -50,3 +50,79 @@ function typewriter(){
     }
 }
 typewriter()
+
+
+
+// #############
+function CopFun() {
+    navigator.clipboard.writeText("https://github.com/InstaPy/InstaPy");
+    var copyButton = document.getElementById("copyfortexta");
+    
+    // Change button text to "link copied"
+    copyButton.innerHTML = "link copied";
+
+    // Reset button text after 4 seconds
+    setTimeout(function() {
+        copyButton.innerHTML = "copylink";
+    }, 4000);
+}
+function CopFuna() {
+    navigator.clipboard.writeText("https://github.com/Sandeepkumar136/JavaScript_Time_CountDown");
+    var copyButton = document.getElementById("copyfortextb");
+    
+    // Change button text to "link copied"
+    copyButton.innerHTML = "link copied";
+
+    // Reset button text after 4 seconds
+    setTimeout(function() {
+        copyButton.innerHTML = "copylink";
+    }, 4000);
+}
+function CopFunb() {
+    navigator.clipboard.writeText("https://github.com/Sandeepkumar136/jarvesai");
+    var copyButton = document.getElementById("copyfortextc");
+    
+    // Change button text to "link copied"
+    copyButton.innerHTML = "link copied";
+
+    // Reset button text after 4 seconds
+    setTimeout(function() {
+        copyButton.innerHTML = "copylink";
+    }, 4000);
+}
+function CopFunc() {
+    navigator.clipboard.writeText("https://qoute-media.firebaseapp.com/");
+    var copyButton = document.getElementById("copyfortextd");
+    
+    // Change button text to "link copied"
+    copyButton.innerHTML = "link copied";
+
+    // Reset button text after 4 seconds
+    setTimeout(function() {
+        copyButton.innerHTML = "copylink";
+    }, 4000);
+}
+function CopFund() {
+    navigator.clipboard.writeText("https://to-do-list-975b5.firebaseapp.com/");
+    var copyButton = document.getElementById("copyfortexte");
+    
+    // Change button text to "link copied"
+    copyButton.innerHTML = "link copied";
+
+    // Reset button text after 4 seconds
+    setTimeout(function() {
+        copyButton.innerHTML = "copylink";
+    }, 4000);
+}
+function CopFune() {
+    navigator.clipboard.writeText("https://portfolio-1ce19.web.app/");
+    var copyButton = document.getElementById("copyfortextf");
+    
+    // Change button text to "link copied"
+    copyButton.innerHTML = "link copied";
+
+    // Reset button text after 4 seconds
+    setTimeout(function() {
+        copyButton.innerHTML = "copylink";
+    }, 4000);
+}
